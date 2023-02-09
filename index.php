39db3ace0d7ca364e9d5ec77d215a79a0948d959
Saeed Noori <noori.saeed1375@gmail.com>
2023-02-09 23:52:58 +0330

    Add webhook urls

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
 
+require_once 'vendor/autoload.php';
 require_once 'autoload.php';
 
 if(!defined('ABSPATH')) define('ABSPATH', __DIR__);
 
+use Longman\TelegramBot\Telegram;
+use Utilities\Helpers;
 use Utilities\Router;
+
+global $telegram;
+$telegram = new Telegram(Helpers::config('bot_token'));
 
 $router = new Router();
 $router->setNamespace('\App');
 
 $router->get('/', 'Bot@index');
+$router->get('/set-webhook', 'Bot@setWebhook');
+$router->post('/webhook', 'Bot@webhook');
 
 $router->set404(function(){
     header('HTTP/1.1 404 Not Found');
